c9c3d8b16cec3f46542000fe7bd6a034a5e3a12e
Jui <jui.pitale@gmail.com>
2021-08-18 16:24:17 +0530

    New function add added

--- MyJavaApp/src/com/ofss/HelloWorld.java
+++ MyJavaApp/src/com/ofss/HelloWorld.java
@@ -1,10 +1,15 @@
 package com.ofss;
 
 public class HelloWorld {
+	
+	public static Integer add(Integer a,Integer b) {
+		return a+b;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!!!");
+		System.out.println(add(6,7));
 	}
 
 }
